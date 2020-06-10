d2236f30b971f40d01082b14aae94d4466305668
Michele Righetti <michele.righetti1998@gmail.com>
2020-06-10 12:20:32 +0200

    .env optional for unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Symfony\Component\Dotenv\Dotenv();
-$dotenv->load(__DIR__ . '/../.env');
+// .env contains the bot token used to perform tests against Telegram Api. This kind of tests are not meant to
+// be executed with CI-tools like TravisCI: they aren't unit-tests since they rely on external resources
+$dotEnv = __DIR__ . '/../.env';
+if (file_exists($dotEnv)) {
+    $dotenv = new Symfony\Component\Dotenv\Dotenv();
+    $dotenv->load($dotEnv);
+}
